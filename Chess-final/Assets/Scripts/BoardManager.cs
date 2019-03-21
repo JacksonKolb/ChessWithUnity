3a099cd30f76bbf362920577108eb578dd8545a2
BiggusDickus420 <faezamin@gmail.com>
2019-03-20 19:35:25 -0600

    Chess Pieces spawned correctly

---
+++ Chess-final/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private const float TILE_SIZE = 1.9f;       //parameters of the tile size
+    private const float TILE_OFFSET = 0.5f;
+
+    private int selectionX = -1;                //Keeps track of current place on board
+    private int selectionY = -1;
+
+    public List<GameObject> chesspiecePrefabs;
+    private List<GameObject> activeChesspiece;
+
+    private Quaternion orientation = Quaternion.Euler(0, 360, 0);
+
+    private void Start()
+    {
+        SpawnAllChessPieces();
+    }
+
+    private void Update()
+    {
+        UpdateSelection();
+        DrawChessboard();
+    }
+
+    private void UpdateSelection()
+    {
+        if (!Camera.main)                       //to test this function
+            return;
+
+        RaycastHit hit;
+       if(Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("ChessPlane")))
+        {
+            selectionX = (int)hit.point.x;
+            selectionY = (int)hit.point.z;
+        }
+        else
+        {
+            selectionX = -1;
+            selectionY = -1;
+        }
+    }
+
+    private void SpawnChessPiece(int index, Vector3 position)
+    {
+        GameObject go = Instantiate(chesspiecePrefabs[index], position, orientation) as GameObject;
+        go.transform.SetParent(transform);
+        activeChesspiece.Add(go);
+    }
+
+    private void SpawnAllChessPieces()
+    {
+        activeChesspiece = new List<GameObject>();
+
+        //Spawn White Pieces
+
+        //King
+        SpawnChessPiece(0, GetTileCenter(4, 0));
+
+        //Queen
+        SpawnChessPiece(1, GetTileCenter(3, 0));
+
+        //Rooks
+        SpawnChessPiece(2, GetTileCenter(0, 0));
+        SpawnChessPiece(2, GetTileCenter(7, 0));
+
+        //Bishops
+        SpawnChessPiece(3, GetTileCenter(2, 0));
+        SpawnChessPiece(3, GetTileCenter(5, 0));
+
+        //Knights
+        SpawnChessPiece(4, GetTileCenter(1, 0));
+        SpawnChessPiece(4, GetTileCenter(6, 0));
+
+        //Pawns
+        for (int i = 0; i < 8; i++)
+            SpawnChessPiece(5, GetTileCenter(i, 1));
+
+        //Spawn Black Pieces
+
+        //King
+        SpawnChessPiece(6, GetTileCenter(3, 7));
+
+        //Queen
+        SpawnChessPiece(7, GetTileCenter(4, 7));
+
+        //Rooks
+        SpawnChessPiece(8, GetTileCenter(0, 7));
+        SpawnChessPiece(8, GetTileCenter(7, 7));
+
+        //Bishops
+        SpawnChessPiece(9, GetTileCenter(2, 7));
+        SpawnChessPiece(9, GetTileCenter(5, 7));
+
+        //Knights
+        SpawnChessPiece(10, GetTileCenter(1, 7));
+        SpawnChessPiece(10, GetTileCenter(6, 7));
+
+        //Pawns
+        for (int i = 0; i < 8; i++)
+            SpawnChessPiece(11, GetTileCenter(i, 6));
+    }
+
+    private Vector3 GetTileCenter(int x, int y)
+    {
+        Vector3 origin = Vector3.zero;
+        origin.x += (TILE_SIZE * x) + TILE_OFFSET;
+        origin.z += (TILE_SIZE * y) + TILE_OFFSET;
+        return origin;
+    }
+
+    private void DrawChessboard()
+    {
+        Vector3 widthLine = Vector3.right * 8;
+        Vector3 heightLine = Vector3.forward * 8;
+
+        for(int i = 0; i <= 8; i++)
+        {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + widthLine);
+            for (int j = 0; j <= 8; j++)
+            {
+                start = Vector3.right * j;
+                Debug.DrawLine(start, start + heightLine);
+            }
+        }
+
+        //Draw the Selection
+        if(selectionX >= 0 && selectionY >= 0)
+        {
+            Debug.DrawLine(
+                Vector3.forward * selectionY + Vector3.right * selectionX,
+                Vector3.forward * (selectionY + 1) + Vector3.right * (selectionX + 1));
+
+            Debug.DrawLine(
+                Vector3.forward * (selectionY + 1)+ Vector3.right * selectionX,
+                Vector3.forward * selectionY + Vector3.right * (selectionX + 1));
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# ChessWithUnity
+ChessWithUnity is a multiplayer chess game that utilizes the Unity engine for graphics.
+
+## Contributers
+@jacksonkolb94
+
+@MaxMorrison6
+
+@BiggusDickus420
+
+# Team Agreement
+This section is intended to document the process that we've all agreed to.
+
+## Ticketing Work In Jira
+* All work is ticketed. If your current work doesn't fall into the scope of a ticket, ask Jack if a new ticket should
+be created.
+
+* All tickets have a point assigned to them, that the team agrees on. If anyone disagrees with a point sizing, we must
+discuss until everyone agrees.
+
+* As a general rule, we should avoid resizing tickets and adding more work to them after their initial creation.
+
+* Branch names must be in this format: `SB-1`, `SB-10`, `SB-100` (SB = Sons of Bishops)
+
+* Branch names must match the specific ticket number in our trello board.
+
+## Code Review
+* Each ticket's PR must be reviewed by the other two members of the team before it is merged.
+
+* To approve a PR, we will leave comments that say `+1`
+
+
